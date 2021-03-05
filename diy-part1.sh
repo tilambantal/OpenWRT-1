17ce74c739651223f69e64a87a0444f13431b837
SolomonRicky <53608272+solomonricky@users.noreply.github.com>
2021-03-06 02:52:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,4 +6,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
+sed -i '$a src-git argontheme https://github.com/jerrykuku/luci-theme-argon' feeds.conf.default
